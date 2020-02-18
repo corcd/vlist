9e98ec5072bfeaf3ddb93326456939487b1d013f
王含泽 <whzcorcd@gmail.com>
2020-02-18 10:46:05 +0800

    [FE] (init) 1. 项目初始化

---
+++ src/components/Item.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>
+    <span style="color:red">{{ item.id + 1 }}</span
+    >{{ item.value + item.value }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  width: 100%;
+
+  span {
+    margin-right: 5px;
+  }
+}
+</style>
